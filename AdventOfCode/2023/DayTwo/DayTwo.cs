12782b2cfd7965896c7183b711a47a5a62fa331c
Isaac Mayes <github@mayesid.com>
2023-12-01 23:28:36 -0600

    Solve day 2 part 2.

--- AdventOfCode/2023/DayTwo/DayTwo.cs
+++ AdventOfCode/2023/DayTwo/DayTwo.cs
@@ -1,5 +1,4 @@
 
-using System.Runtime.Intrinsics.Arm;
 using System.Text.RegularExpressions;
 
 namespace _2023
@@ -19,47 +18,83 @@ namespace _2023
                         continue;
                     }
 
-                    //Game 3: 6 green, 15 red; 1 green, 4 red, 7 blue; 9 blue, 7 red, 8 green
-                    // 12 R
-                    // 13 G
-                    // 14 B
-                    var maxRed = 12;
-                    var maxGreen = 13;
-                    var maxBlue = 14;
-                    var gameInfo = line.Split(':',StringSplitOptions.TrimEntries);
-                    var gameID = int.Parse(Regex.Match(gameInfo[0],"\\d+").Value);
-                    var gameDetails = gameInfo[1].Split(';',StringSplitOptions.TrimEntries);
-                    bool isPossible = true;
-                    foreach(var detail in gameDetails){
-                        if(!isPossible)
-                        {
-                            break;
-                        }
-                        var roundDetails = detail.Split(',',StringSplitOptions.TrimEntries);
-                        foreach(var rDetail in roundDetails)
-                        {
-                            if(!isPossible)
-                            {
-                                break;
-                            }
-                            if(rDetail.Contains("red") && int.Parse(Regex.Match(rDetail,"\\d+").Value) > maxRed)
-                            {
-                                isPossible = false;
-                            }
-                            if(rDetail.Contains("green")&& int.Parse(Regex.Match(rDetail,"\\d+").Value) > maxGreen)
-                            {
-                                isPossible = false;
-                            }
-                            if(rDetail.Contains("blue")&& int.Parse(Regex.Match(rDetail,"\\d+").Value) > maxBlue)
-                            {
-                                isPossible = false;
-                            }
-                        }
-                    }
-                    if(isPossible) {total+=gameID;}
+                    // total += SolvePartOne(line);
+                    total += SolvePartTwo(line);
                 }
                 return total.ToString();
             }
+        }
+
+        private static int SolvePartOne(string line)
+        {
+            var maxRed = 12;
+            var maxGreen = 13;
+            var maxBlue = 14;
+            var gameInfo = line.Split(':', StringSplitOptions.TrimEntries);
+            var gameID = int.Parse(Regex.Match(gameInfo[0], "\\d+").Value);
+            var gameDetails = gameInfo[1].Split(';', StringSplitOptions.TrimEntries);
+            bool isPossible = true;
+            foreach (var detail in gameDetails)
+            {
+                if (!isPossible)
+                {
+                    break;
+                }
+                var roundDetails = detail.Split(',', StringSplitOptions.TrimEntries);
+                foreach (var rDetail in roundDetails)
+                {
+                    if (!isPossible)
+                    {
+                        break;
+                    }
+                    if (rDetail.Contains("red") && int.Parse(Regex.Match(rDetail, "\\d+").Value) > maxRed)
+                    {
+                        isPossible = false;
+                    }
+                    if (rDetail.Contains("green") && int.Parse(Regex.Match(rDetail, "\\d+").Value) > maxGreen)
+                    {
+                        isPossible = false;
+                    }
+                    if (rDetail.Contains("blue") && int.Parse(Regex.Match(rDetail, "\\d+").Value) > maxBlue)
+                    {
+                        isPossible = false;
+                    }
+                }
+            }
+            if (isPossible) { return gameID; }
+            else { return 0; }
+        }
+
+        private static int SolvePartTwo(string line)
+        {
+            var maxRed = 0;
+            var maxGreen = 0;
+            var maxBlue = 0;
+            var gameInfo = line.Split(':', StringSplitOptions.TrimEntries);
+            var gameID = int.Parse(Regex.Match(gameInfo[0], "\\d+").Value);
+            var gameDetails = gameInfo[1].Split(';', StringSplitOptions.TrimEntries);
+
+            foreach (var detail in gameDetails)
+            {
+                var roundDetails = detail.Split(',', StringSplitOptions.TrimEntries);
+                foreach (var rDetail in roundDetails)
+                {
+                    if (rDetail.Contains("red") && int.TryParse(Regex.Match(rDetail, "\\d+").Value, out int redCount) && redCount>maxRed)
+                    {
+                        maxRed = redCount;
+                    }
+                    if (rDetail.Contains("green") && int.TryParse(Regex.Match(rDetail, "\\d+").Value, out int greenCount) && greenCount>maxGreen)
+                    {
+                        maxGreen = greenCount;
+                    }
+                    if (rDetail.Contains("blue") && int.TryParse(Regex.Match(rDetail, "\\d+").Value, out int blueCount) && blueCount>maxBlue)
+                    {
+                        maxBlue = blueCount;
+                    }
+                }
+            }
+
+            return maxRed*maxGreen*maxBlue;
         }
     }
 }
