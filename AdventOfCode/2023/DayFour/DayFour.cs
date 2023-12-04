a45bf9b6a6964b28865fa7794ceb936d7bbcbe1e
Isaac Mayes <github@mayesid.com>
2023-12-04 00:16:41 -0600

    Solve day 4 part 2.

--- AdventOfCode/2023/DayFour/DayFour.cs
+++ AdventOfCode/2023/DayFour/DayFour.cs
@@ -8,7 +8,9 @@ namespace _2023
         {
             using (StreamReader reader = File.OpenText(filePath))
             {
-                var total = 0d;
+                var total = 0;
+                Dictionary<int, int> scoreCopies = [];
+                var lineCount = 0;
                 while (!reader.EndOfStream)
                 {
                     string? line = reader.ReadLine();
@@ -16,30 +18,55 @@ namespace _2023
                     {
                         continue;
                     }
+                    lineCount++;
+                    var firstSplit = line.Split(':', StringSplitOptions.TrimEntries);
+                    var card = int.Parse(Regex.Match(firstSplit[0], "\\d+").Value);
+                    var numbers = firstSplit[1].Split('|', StringSplitOptions.TrimEntries);
+                    var winningNumbers = numbers[0].Split(' ', StringSplitOptions.TrimEntries).Where(entry => !entry.Equals(string.Empty));
+                    var myNumbers = numbers[1].Split(' ', StringSplitOptions.TrimEntries).Where(entry => !entry.Equals(string.Empty));
 
-                    var firstSplit = line.Split(':',StringSplitOptions.TrimEntries);
-                    var card = Regex.Match(firstSplit[0],"\\d+");
-                    var numbers = firstSplit[1].Split('|',StringSplitOptions.TrimEntries);
-                    var winningNumbers = numbers[0].Split(' ',StringSplitOptions.TrimEntries).Where(entry=>!entry.Equals(string.Empty));
-                    var myNumbers = numbers[1].Split(' ',StringSplitOptions.TrimEntries).Where(entry=>!entry.Equals(string.Empty));
-
-                    Console.WriteLine($"Card: {card}, winning numbers {winningNumbers.Select(winNum=>winNum).ToList()}.");
-
-                    var points = 0d;
-                    var scoreMod = 0;
-                    foreach(var myNum in myNumbers)
+                    var numCopies = scoreCopies.TryGetValue(card, out int outCopies) ? outCopies + 1 : 1;
+                    while (numCopies > 0)
                     {
-                        if(winningNumbers.Any(wNum => wNum.Equals(myNum)))
+                        // Console.WriteLine($"Card: {card}, winning numbers {winningNumbers.Aggregate((string1, string2) => $"{string1},{string2}")}.");
+                        var numMatches = 1;
+                        foreach (var myNum in myNumbers)
                         {
-                            points = Math.Pow(2,scoreMod);
-                            Console.WriteLine($"Matched {myNum} for {points} points.");
-                            scoreMod++;
+                            if (winningNumbers.Any(wNum => wNum.Equals(myNum)))
+                            {
+                                var copyIndex = card + numMatches;
+                                if (!scoreCopies.TryGetValue(copyIndex, out int copyCount))
+                                {
+                                    copyCount = 0;
+                                }
+                                scoreCopies[copyIndex] = ++copyCount;
+
+                                // Console.WriteLine($"Match! Card: {copyIndex}, CopyCount: {copyCount}");
+                                numMatches++;
+                            }
                         }
+                        numCopies--;
                     }
-                    total += points;
                 }
+                total = lineCount + scoreCopies.Values.Sum();
                 return total.ToString();
             }
+        }
+
+        private static double ScorePartOne(IEnumerable<string> myNumbers, IEnumerable<string> winningNumbers)
+        {
+            var points = 0d;
+            var scoreMod = 0;
+            foreach (var myNum in myNumbers)
+            {
+                if (winningNumbers.Any(wNum => wNum.Equals(myNum)))
+                {
+                    points = Math.Pow(2, scoreMod);
+                    Console.WriteLine($"Matched {myNum} for {points} points.");
+                    scoreMod++;
+                }
+            }
+            return points;
         }
     }
 }
