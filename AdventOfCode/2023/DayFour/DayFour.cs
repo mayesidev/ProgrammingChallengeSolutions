0d3b5d19642fce32939862841881dea2f9753071
Isaac Mayes <github@mayesid.com>
2023-12-03 23:24:48 -0600

    Solve day 4 part 1.

--- AdventOfCode/2023/DayFour/DayFour.cs
+++ AdventOfCode/2023/DayFour/DayFour.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace _2023
 {
     public class DayFour
@@ -6,7 +8,7 @@ namespace _2023
         {
             using (StreamReader reader = File.OpenText(filePath))
             {
-                var total = 0;
+                var total = 0d;
                 while (!reader.EndOfStream)
                 {
                     string? line = reader.ReadLine();
@@ -15,7 +17,26 @@ namespace _2023
                         continue;
                     }
 
-                    // total += ParseLine(line);
+                    var firstSplit = line.Split(':',StringSplitOptions.TrimEntries);
+                    var card = Regex.Match(firstSplit[0],"\\d+");
+                    var numbers = firstSplit[1].Split('|',StringSplitOptions.TrimEntries);
+                    var winningNumbers = numbers[0].Split(' ',StringSplitOptions.TrimEntries).Where(entry=>!entry.Equals(string.Empty));
+                    var myNumbers = numbers[1].Split(' ',StringSplitOptions.TrimEntries).Where(entry=>!entry.Equals(string.Empty));
+
+                    Console.WriteLine($"Card: {card}, winning numbers {winningNumbers.Select(winNum=>winNum).ToList()}.");
+
+                    var points = 0d;
+                    var scoreMod = 0;
+                    foreach(var myNum in myNumbers)
+                    {
+                        if(winningNumbers.Any(wNum => wNum.Equals(myNum)))
+                        {
+                            points = Math.Pow(2,scoreMod);
+                            Console.WriteLine($"Matched {myNum} for {points} points.");
+                            scoreMod++;
+                        }
+                    }
+                    total += points;
                 }
                 return total.ToString();
             }
