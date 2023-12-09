00af4730f1a47f983841e600e4a9e589465d21ae
Isaac Mayes <github@mayesid.com>
2023-12-09 00:06:41 -0600

    Solve day 9 part 1.

--- AdventOfCode/2023/DayNine/DayNine.cs
+++ AdventOfCode/2023/DayNine/DayNine.cs
@@ -1,10 +1,14 @@
+using System.Diagnostics;
+
 namespace _2023
 {
     public class DayNine
     {
         public static string Solve(string filePath)
         {
-            var total = 0;
+            var total = 0L;
+            Dictionary<int, IEnumerable<long>> sequences = [];
+            var lineIndex = 1;
             using (StreamReader reader = File.OpenText(filePath))
             {
                 while (!reader.EndOfStream)
@@ -15,10 +19,45 @@ namespace _2023
                         continue;
                     }
 
-                    // total += ParseLine(line);
+                    sequences.Add(lineIndex, line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse));
+                    lineIndex++;
                 }
             }
+
+            foreach (var sequence in sequences)
+            {
+                Dictionary<int, IEnumerable<long>> subsequences = [];
+                var currentSequence = sequence.Value.ToArray();
+                var currentIndex = 1;
+                while (!currentSequence.All(val => val == 0))
+                {
+                    var nextSequence = CalcSequenceDiff(currentSequence);
+                    subsequences.Add(currentIndex, nextSequence);
+                    currentSequence = nextSequence.ToArray();
+                    currentIndex++;
+
+                    //Debug.WriteLine($"Line: {sequence.Key} Subsequence: {currentIndex} Length: {nextSequence.Count()}");
+                }
+
+                var previouslyAddedValue = 0L;
+                for (var i = subsequences.Count - 1; i > 0; i--)
+                {
+                    previouslyAddedValue += subsequences[i].Last();
+                    // Debug.WriteLine($"Line: {sequence.Key} Subsequence: {i} Added: {previouslyAddedValue}");
+                }
+                total += sequence.Value.Last() + previouslyAddedValue;
+            }
             return total.ToString();
+        }
+
+        public static IEnumerable<long> CalcSequenceDiff(long[] sequence)
+        {
+            List<long> subsequence = [];
+            for (var i = 0; i < sequence.Length - 1; i++)
+            {
+                subsequence.Add(sequence[i + 1] - sequence[i]);
+            }
+            return subsequence;
         }
     }
 }
