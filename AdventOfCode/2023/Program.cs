bbf7ff99a40000ebb7b305d3c31c22b19dfe8f83
Isaac Mayes <github@mayesid.com>
2023-12-11 23:08:43 -0600

    Prep day 12.

--- AdventOfCode/2023/Program.cs
+++ AdventOfCode/2023/Program.cs
@@ -10,14 +10,15 @@ namespace _2023
             // Console.WriteLine(DayFour.Solve("./DayFour/input.txt"));
             // var dayFive = new DayFive():
             // var dayFive = new DayFiveReverse();
-            // dayFive.Solve("./DayFive/input.txt");
+            // TODO: dayFive.Solve("./DayFive/input.txt");
             // Console.WriteLine(DaySix.SolvePart2("./DaySix/input.txt"));
             // var daySeven = new DaySeven();
             // Console.WriteLine(daySeven.Solve("./DaySeven/input.txt"));
-            // Console.WriteLine(DayEight.Solve("./DayEight/input.txt"));
+            // TODO: Console.WriteLine(DayEight.Solve("./DayEight/input.txt"));
             // Console.WriteLine(DayNine.Solve("./DayNine/input.txt"));
-            Console.WriteLine(DayTen.Solve("./DayTen/input.txt"));
+            // TODO: Console.WriteLine(DayTen.Solve("./DayTen/input.txt"));
             // Console.WriteLine(DayEleven.Solve("./DayEleven/input.txt"));
+            Console.WriteLine(DayTwelve.Solve("./DayTwelve/input.txt"));
         }
     }
 }
